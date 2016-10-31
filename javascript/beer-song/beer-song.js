107b5b5783c7633d11f71fe52f60fd5309dcf135
omgjwood <justinemwood@gmail.com>
2016-10-31 01:07:46 -0500

    created the beer-song js file.  Will be adding more as i go along.

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,20 @@
+/*var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+
+    var name = input || 'World';
+    return  'Hello, ' + name + '!';
+};
+
+module.exports = HelloWorld; */
+
+
+
+var beerSong = function () {};
+
+BeerSong.prototype.beer = function(input) {
+
+    var beer= 100;
+        for( var i = beer.length -1; i>= 0; i--);
+        
+}
